14f4c556b11502f277f2a55607eb2b01fcf28099
Fizvlad <fizvlad@mail.ru>
2019-06-13 17:37:14 +0300

    Started working on main functions

--- lib/vk_music/audio.rb
+++ lib/vk_music/audio.rb
@@ -1,23 +1,47 @@
 module VkMusic
 
+  def self.unmask_link(link, client_id)
+    # TODO
+    return link
+  end
+
   class Audio
-  
-=begin
-  Info about audio (from API):
-    "id": 456240311,
-    "owner_id": 157230821,
-    "artist": "МЦ Райд",
-    "title": "Пытается выехать из Омска (но уже молча)",
-    "duration": 213,
-    "date": 1560000141,
-    "url": "https://cs9-7v4.v...aeQwd5sNX3uVsnZ28Yt",
-    "is_hq": true,
-    "track_code": "5e8e4565txz982pOCxUCMM3ZD84",
-    "is_explicit": false
-=end    
+    # Attributes
+    attr_reader :id, :owner_id, :artist, :title, :duration, :url, :url_encoded
+    
+    def to_s
+      "#{@artist} - #{@title} [#{Utility.format_seconds(@duration)}]"
+    end
   
     def initialize(options)
-      # TODO
+      # Arguments check
+      raise ArgumentError, "options hash must be provided", caller unless options.class == Hash
+      raise ArgumentError, "artist is not provided", caller unless options.has_key?(:artist)
+      raise ArgumentError, "title is not provided", caller unless options.has_key?(:title)
+      raise ArgumentError, "duration is not provided", caller unless options.has_key?(:duration)
+      raise ArgumentError, "url is not provided", caller unless options.has_key?(:url)
+      
+      # Setting up attributes
+      @id          = options[:id].to_s
+      @owner_id    = options[:owner_id].to_s
+      @artist      = options[:artist].to_s
+      @title       = options[:title].to_s
+      @duration    = options[:duration].to_i
+      @url_encoded = options[:url_encoded].to_s
+      @url         = options[:url].to_s
+    end
+    
+    def self.from_node(node, client_id)
+      puts node
+      new({
+        :id => node.attribute("data-id").to_s,
+        :owner_id => "", # TODO
+        :artist => node.at_css(".ai_artist").text.strip,
+        :title => node.at_css(".ai_title").text.strip,
+        :duration => node.at_css(".ai_dur").attribute("data-dur").to_s.to_i,
+        :url_encoded => node.at_css("input").attribute("value").to_s,
+        :url => VkMusic.unmask_link(@url_encoded, client_id),
+      })
     end
   
   end
--- lib/vk_music/client.rb
+++ lib/vk_music/client.rb
@@ -1,19 +1,34 @@
+require "mechanize"
+
 module VkMusic
 
   class Client
   
-    # TODO: Must store user id and cookies, but NOT log/pass
+    # User id and name
+    attr_reader :id, :name
+    
+    # Mechanize agent
+    @agent = nil
   
     def initialize(options)
       # Arguments check
-      raise ArgumentError, "Username is not provided", caller unless options.has_key?(:username)
-      raise ArgumentError, "Password is not provided", caller unless options.has_key?(:password)
+      raise ArgumentError, "options hash must be provided", caller unless options.class == Hash
+      raise ArgumentError, "username is not provided", caller unless options.has_key?(:username)
+      raise ArgumentError, "password is not provided", caller unless options.has_key?(:password)
       
-      # TODO
+      # Setting up client
+      @agent = Mechanize.new
+      login(options[:username], options[:password])
     end
     
     def find_audio(query)
-      # TODO
+      # Loading page
+      uri = URI(VK_URL[:audios])
+      uri.query = URI.encode_www_form("act" => "search", "q" => query.to_s)
+      page = load_page(uri)
+      
+      # Parsing audio elements
+      audios = page.css(".audio_item.ai_has_btn").map { |elem| Audio.from_node(elem, @id) }
     end
     
     def get_playlist(url)
@@ -21,14 +36,39 @@ module VkMusic
     end
     
     private
-    def login
-      # TODO
+    def load_page(url)
+      # Need URI object
+      uri = URI(url) if url.class != URI
+      
+      # Arguments check
+      raise ArgumentError, "HTTPS scheme required", caller unless uri.scheme == VK_URL[:scheme]
+      raise ArgumentError, "this method is only used to interact with #{VK_URL[:host]}", caller unless uri.host == VK_URL[:host]
+      
+      @agent.get(url)
+    end
+    
+    def login(username, password)
+      # Loading login page
+      homepage = load_page(VK_URL[:home])
+      # Submitting login form
+      login_form = homepage.forms.find { |form| form.action.start_with?(VK_URL[:login_action]) }
+      login_form[VK_LOGIN_FORM_NAMES[:username]] = username.to_s
+      login_form[VK_LOGIN_FORM_NAMES[:password]] = password.to_s
+      after_login = @agent.submit(login_form)
+      
+      # Checking whether logged in
+      raise LoginError, "unable to login. Redirected to #{after_login.uri.to_s}", caller unless after_login.uri.to_s == VK_URL[:feed]
+      
+      # Parsing information about this profile
+      profile = load_page(VK_URL[:profile])      
+      @name = profile.title
+      @id = profile.link_with(href: /audios/).href.slice(/\d+/)
     end
     
     def unmask_link(link)
-      # TODO
+      VkMusic.unmask_link(link, @id)
     end
-  
+    
   end
   
 end
