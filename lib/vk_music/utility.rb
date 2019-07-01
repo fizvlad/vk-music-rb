21c066a5c179c93522b85e8194971ebff9d5b205
Fizvlad <fizvlad@mail.ru>
2019-07-01 16:22:17 +0300

    Added function to guess type of request string
    Added function to guess type of request string

--- lib/vk_music/utility.rb
+++ lib/vk_music/utility.rb
@@ -7,6 +7,17 @@ module VkMusic
       "#{(s / 60).to_s.rjust(2, "0")}:#{(s % 60).to_s.rjust(2, "0")}";
     end
     
+    def self.guess_request_type(str)
+      # Guess what type of request is this. Returns Symbol: :find, :playlist, :audios
+      if str.match? PLAYLIST_URL_REGEX
+        :playlist
+      elsif str.match? VK_URL_REGEX
+        :audios
+      else
+        :find
+      end
+    end
+    
   end
   
 end
