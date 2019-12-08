962f87e9f4b30732456c4a2a8190e86b2a46d95e
Fizvlad <fizvlad@mail.ru>
2019-12-08 21:05:07 +0300

    use require_relative instead of regular require

--- lib/vk_music.rb
+++ lib/vk_music.rb
@@ -4,14 +4,14 @@ require "mechanize"
 require "execjs"
 require "json"
 
-require "vk_music/version"
-require "vk_music/constants"
-require "vk_music/exceptions"
-require "vk_music/utility"
-require "vk_music/link_decoder"
-require "vk_music/audio"
-require "vk_music/playlist"
-require "vk_music/client"
+require_relative "vk_music/version"
+require_relative "vk_music/constants"
+require_relative "vk_music/exceptions"
+require_relative "vk_music/utility"
+require_relative "vk_music/link_decoder"
+require_relative "vk_music/audio"
+require_relative "vk_music/playlist"
+require_relative "vk_music/client"
 
 ##
 # Main module.
