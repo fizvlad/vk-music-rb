6443daafea7d8f4edf9554c76d98254ebbd99968
Fizvlad <fizvlad@mail.ru>
2020-11-06 15:23:48 +0300

    fix: requires

--- lib/vk_music.rb
+++ lib/vk_music.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # Main module
 module VkMusic; end
 
 require_relative 'vk_music/version'
-require_relative 'vk_music/utility/duration_parser'
+require_relative 'vk_music/utility'
+require_relative 'vk_music/client'
+require_relative 'vk_music/audio'
+require_relative 'vk_music/playlist'
--- lib/vk_music/utility.rb
+++ lib/vk_music/utility.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'utility/audio_data_parser'
+require_relative 'utility/audio_node_parser'
 require_relative 'utility/duration_parser'
+require_relative 'utility/node_text_children_reader'
 
 module VkMusic
   # Helpers
