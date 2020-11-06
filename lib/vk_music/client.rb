cd2a7014b70e7bbcf19ed59cf376cf3b1488ff45
Fizvlad <fizvlad@mail.ru>
2020-11-06 22:51:41 +0300

    wip: logging in

--- lib/vk_music/client.rb
+++ lib/vk_music/client.rb
@@ -3,6 +3,35 @@
 module VkMusic
   # VK client
   class Client
-    # TODO
+    # Default user agent to use
+    DEFAULT_USERAGENT = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) ' \
+                        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                        'Chrome/86.0.4240.111 Mobile Safari/537.36'
+    public_constant :DEFAULT_USERAGENT
+
+    # @return [Integer] ID of client
+    attr_reader :id
+    # @return [String] name of client
+    attr_reader :name
+    # @return [Mechanize] client used to access web pages
+    attr_reader :agent
+
+    # @param login [String]
+    # @param password [String]
+    def initialize(login:, password:, user_agent: DEFAULT_USERAGENT)
+      @login = login
+      @password = password
+
+      @agent = Mechanize.new
+      @agent.user_agent = user_agent
+
+      self.login
+    end
+
+    # Make a login request
+    def login
+      VkMusic.log.info("Client#{@id}") { 'Logging in...' }
+      # TODO
+    end
   end
 end
