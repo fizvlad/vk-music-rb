efe66b1e5c8c2d2274885f989ba776999410b7e7
Fizvlad <fizvlad@mail.ru>
2020-11-06 20:33:40 +0300

    fix: requiring utility classes

--- lib/vk_music/utility.rb
+++ lib/vk_music/utility.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require_relative 'utility/audio_data_parser'
-require_relative 'utility/audio_node_parser'
-require_relative 'utility/duration_parser'
-require_relative 'utility/node_text_children_reader'
-
 module VkMusic
   # Helpers
   module Utility; end
 end
+
+require_relative 'utility/audio_data_parser'
+require_relative 'utility/audio_node_parser'
+require_relative 'utility/duration_parser'
+require_relative 'utility/node_text_children_reader'
