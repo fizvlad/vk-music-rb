e8524fc1317c00a52420b269673a514761a86543
Fizvlad <fizvlad@mail.ru>
2020-11-08 00:16:03 +0300

    feat: web parsers

---
+++ lib/vk_music/web_parser.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module VkMusic
+  # Parses out any data from page received by {Request} objects
+  module WebParser; end
+end
+
+require_relative 'web_parser/base'
+require_relative 'web_parser/login'
+require_relative 'web_parser/my_page'
+require_relative 'web_parser/search'
