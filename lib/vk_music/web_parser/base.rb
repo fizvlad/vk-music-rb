56293c787ac203368e30708709e988c1c9e7365e
Fizvlad <fizvlad@mail.ru>
2020-11-08 11:59:03 +0300

    feat: allow storing client id in parsers

--- lib/vk_music/web_parser/base.rb
+++ lib/vk_music/web_parser/base.rb
@@ -5,8 +5,10 @@ module VkMusic
     # Base class for all web parsers
     class Base
       # @param obj [String, Nokogiri::XML::Searchable]
-      def initialize(obj)
+      # @param client_id [Integer?]
+      def initialize(obj, client_id: nil)
         @node = obj.is_a?(String) ? Nokogiri::HTML.fragment(obj) : obj
+        @client_id = client_id
       end
     end
   end
