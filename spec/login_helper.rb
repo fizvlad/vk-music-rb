d30e10dde66871d5e2057fa60db105894f1888eb
Vladislav Kuznecov <fizvlad@mail.ru>
2022-08-30 01:59:03 +0300

    feat: pass login and password into authorizer

--- spec/login_helper.rb
+++ spec/login_helper.rb
@@ -5,7 +5,7 @@ AGENT_COOKIES_PATH = 'spec/.cookies'
 
 # @return [Mechanize] logged in Mechanize client.
 def logged_in_agent
-  VkMusic::Utility::Authorizer.call(AGENT_COOKIES_PATH)
+  VkMusic::Utility::Authorizer.call(ENV['VK_LOGIN'], ENV['VK_PASSWORD'], AGENT_COOKIES_PATH)
 end
 
 # @return [Client] logged in client.
